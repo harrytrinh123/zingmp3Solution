2816f2a09d6a5264bc632854c77e3961380478aa
Jack Trinh <40319325+jacktrinh123@users.noreply.github.com>
2021-08-11 23:42:50 +0700

    [Write] write service and controller

--- zingmp3Solution.WebApi/Startup.cs
+++ zingmp3Solution.WebApi/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using zingmp3Solution.Application.Catalog.Songs;
+using zingmp3Solution.Data.EF;
 
 namespace zingmp3Solution.WebApi
 {
@@ -26,11 +29,15 @@ namespace zingmp3Solution.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ZingMp3DbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ZingMp3SolutionDb")));
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Zingmp3", Version = "v1" });
             });
+            services.AddTransient<ISongService, SongService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
