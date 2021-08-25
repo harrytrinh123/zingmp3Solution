e039623d58d04340ac5874e7e0790826db19837a
Jack Trinh <40319325+jacktrinh123@users.noreply.github.com>
2021-08-25 21:50:02 +0700

    [Write] add interface IPostService

---
+++ zingmp3Solution.Application/Catalog/Posts/IPostService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using zingmp3Solution.Dtos.Catalog.Posts;
+
+namespace zingmp3Solution.Application.Catalog.Posts
+{
+    public interface IPostService
+    {
+        Task<List<PostDto>> GetPaged(int pageIndex, int pageSize);
+        // CRUD
+        Task<PostDto> GetPostById(int postId);
+        Task<int> Create(PostCreateDto request);
+        Task<int> Update(int postId,PostUpdateDto request);
+        Task<int> Delete(int postId);
+    }
+}
---
+++ zingmp3Solution.Application/Catalog/Posts/PostService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using zingmp3Solution.Dtos.Catalog.Posts;
+
+namespace zingmp3Solution.Application.Catalog.Posts
+{
+    public class PostService : IPostService
+    {
+        public Task<int> Create(PostCreateDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PostDto>> GetPaged(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDto> GetPostById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(int postId, PostUpdateDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
